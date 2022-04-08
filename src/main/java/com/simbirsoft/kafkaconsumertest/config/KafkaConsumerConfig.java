6cfe1e0a000a4925c3ce2dbf519222405320bfec
Roman Vershinin <roman.vershinin@simbirsoft.com>
2022-04-08 16:14:31 +0400

    Add project files

---
+++ src/main/java/com/simbirsoft/kafkaconsumertest/config/KafkaConsumerConfig.java
@@ -0,0 +1,47 @@
+package com.simbirsoft.kafkaconsumertest.config;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.simbirsoft.kafkaconsumertest.model.EmployeeDTO;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+@Configuration
+@EnableKafka
+public class KafkaConsumerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String kafkaServer;
+    @Value("${spring.kafka.consumer.group-id}")
+    private String kafkaConsumerGroupId;
+
+    @Bean
+    public KafkaListenerContainerFactory<?> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<Long, EmployeeDTO> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+
+    public ConsumerFactory<Long, EmployeeDTO> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+    }
+
+    private Map<String, Object> consumerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaConsumerGroupId);
+        return props;
+    }
+}
---
+++ src/main/java/com/simbirsoft/kafkaconsumertest/model/Position.java
@@ -0,0 +1,15 @@
+package com.simbirsoft.kafkaconsumertest.model;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+@Data
+public class Position {
+
+    private String name;
+    private Double experience;
+    @JsonFormat(pattern = "dd.MM.yyyy")
+    private LocalDate hireDate;
+}
